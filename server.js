11100549f9f58d6fb0d269f8fe3f715e88ac538b
DragonQ <dragonq29@gmail.com>
2021-06-28 23:33:47 +0900

    포트 8000으로 변경, CORS 문제 수정

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
 // 객체 안에 하위 객체가 존재할때 : extended: true
 app.use(express.urlencoded({extended: true}));
 // 요청 본문이 객체 형식일때 파싱해줌
 app.use(express.json())
-app.post("/todayMenu_nList" , (req , res) => {
+app.use(cors())
+app.post("/smartfood/todaymenuGf/todayMenu_nList_pro.do" , (req , res) => {
     const data = req.body;
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*");
     // const data = {
     //     "end_dt": "20210621",
     //     "st_dt": "20210621",
@@ -14,7 +18,7 @@ app.post("/todayMenu_nList" , (req , res) => {
     // };
 
     axios.post('https://sfv.hyundaigreenfood.com/smartfood/todaymenuGf/todayMenu_nList_pro.do', data)
-    .then(response => res.send({success : true , response: response.data}))
-    .catch(error => res.send({success : false , message: error.message}));
+    .then(response => res.send({...response.data}))
+    .catch(error => res.send({...error.message}));
 })
-app.listen("3001",() => console.log("Server started"))
+app.listen("8000",() => console.log("Server started"))
